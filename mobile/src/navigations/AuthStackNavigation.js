d12954f9706870f8c260874eff9c58fc12225219
S1ngularty <asherxd10245@gmail.com>
2026-03-09 23:17:20 +0800

    register screen initial

--- mobile/src/navigations/AuthStackNavigation.js
+++ mobile/src/navigations/AuthStackNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStaticNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
 
 const authStack = createNativeStackNavigator({
   screens: {
@@ -9,6 +10,10 @@ const authStack = createNativeStackNavigator({
       screen: LoginScreen,
       options: { title: "Welcome back bitch" },
     },
+    Register:{
+      screen: RegisterScreen,
+      options: { title: "Create an account" },
+    }
   },
 });
 
