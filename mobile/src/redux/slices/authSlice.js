c41a1fd380910247aea1c8e621507953590f7820
S1ngularty <asherxd10245@gmail.com>
2026-03-12 23:55:23 +0800

    async thunk for redux state management

--- mobile/src/redux/slices/authSlice.js
+++ mobile/src/redux/slices/authSlice.js
@@ -1,4 +1,11 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { login } from "../thunks/authThunk";
+
+function handleStatus(state){
+  state = {...state, isLogin:false, loading:true}
+  return
+}
+
 
 const authSlice = createSlice({
   name: "auth",
@@ -9,7 +16,9 @@ const authSlice = createSlice({
     error: "",
   },
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder)=>{
+    builder.addCase(login.pending,handleStatus(state))
+  },
 });
 
 export const {} = authSlice.actions;
---
+++ mobile/src/redux/thunks/authThunk.js
@@ -0,0 +1,8 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const login = createAsyncThunk("auth/login", async (credentials) => {
+  // api
+  const result = "";
+  return result;
+});
