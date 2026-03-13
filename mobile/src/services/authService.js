ed776b9426f949b93ed8c32510cf29f4bde442cc
S1ngularty <asherxd10245@gmail.com>
2026-03-13 15:53:16 +0800

    initial auth functions

--- mobile/src/services/authService.js
+++ mobile/src/services/authService.js
@@ -4,7 +4,7 @@ export const login = async (credentials) => {
   if (!credentials.email) throw new Error("missing email");
   if (!credentials.password) throw new Error("missing password");
 
-  const response = await axiosInstance.post("/api/v1/login", {
+  const response = await axiosInstance.post("/api/v1/login", credentials, {
     skipIntercept: true,
   });
 
@@ -15,4 +15,9 @@ export const login = async (credentials) => {
   return response.data.result;
 };
 
+export const register = async (formData = {}) => {
+  const response = await axiosInstance.post("/api/v1/register", formData);
 
+  if (!response?.data?.success) throw new Error(response.data.result.error);
+  return response.data.result;
+};
