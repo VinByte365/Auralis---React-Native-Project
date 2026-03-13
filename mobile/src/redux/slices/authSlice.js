ac3afd997e9281043d1bac506eabc5aa419bde8e
S1ngularty <asherxd10245@gmail.com>
2026-03-13 18:04:56 +0800

    auth state and thunks

--- mobile/src/redux/slices/authSlice.js
+++ mobile/src/redux/slices/authSlice.js
@@ -1,27 +1,45 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
-import { login } from "../thunks/authThunk";
+import { login, register } from "../thunks/authThunk";
 
-function handleStatus(state){
-  state = {...state, isLogin:false, loading:true}
-  return
+function handlePending(state) {
+  state = { ...state, isLoggedIn: false, loading: true };
+  return;
 }
 
-const intialStat = {
-    user: {},
-    isLogin: false,
-    loading: false,
-    error: "",
-  }
+function handleFulfilled(state, payload) {
+  state = { ...state, isLoggedIn: true, loading: true, user: payload.result };
+}
 
+function handleRejected(state, payload) {
+  state = { ...state, loading: false, error: payload.result.error };
+}
+
+const initialState = {
+  user: {},
+  isLoggedIn: false,
+  loading: false,
+  error: "",
+};
 
 const authSlice = createSlice({
   name: "auth",
-  
-  reducers: {},
-  extraReducers: (builder)=>{
-    builder.addCase(login.pending,handleStatus(state))
+  initialState,
+  reducers: {
+    resetState: (state, action) => {
+      state = { ...initialState };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, handlePending(state))
+      .addCase(login.fulfilled, handleFulfilled(state))
+      .addCase(login.rejected, handleRejected(state))
+
+      .addCase(register.pending, handlePending(state))
+      .addCase(register.fulfilled, handleFulfilled(state))
+      .addCase(register.rejected, handleRejected(state));
   },
 });
 
 export const {} = authSlice.actions;
-export default  auth = authSlice.reducer;
+export default auth = authSlice.reducer;
--- mobile/src/redux/thunks/authThunk.js
+++ mobile/src/redux/thunks/authThunk.js
@@ -1,9 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { API_URL } from "../../constants/config";
+import * as authService from "../../services/authService";
+import { useState } from "react";
 
-export const login = createAsyncThunk("auth/login", async (credentials) => {
-  const result = 
-  result = "";
-  return result;
-});
+export const login = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
+    const userState = thunkAPI.getState().auth;
+    if (userState.isLoggedIn) return;
+    if (!credentials) throw new Error("missing credentials");
+    return await authService.login(credentials);
+  },
+);
+
+export const register = createAsyncThunk("auth/register", async(formData,{getState})=>{
+  const userState = getState().auth
+  if(userState.isLoggedIn) return
+  return await authService.register(formData)
+})
